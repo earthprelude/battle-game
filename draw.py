942b2351bdbe5513a7c192e5caf172ea08408c40
prelude <75330343+earthprelude@users.noreply.github.com>
2022-01-17 19:11:50 -0800

    Update draw.py
    Added constants

--- draw.py
+++ draw.py
@@ -1,17 +1,28 @@
 import turtle 
 from player import * 
 
+# Constants
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+USER_X = -160
+USER_Y = -80
+BOT_X = 160 
+BOT_Y = 80
+LUNGE = 50
+SLIDE = 100
+TURN = 45
+
 # set up window
 screen = turtle.Screen()
-screen.setup(400, 400)
-screen.bgcolor("gray")
+screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.bgcolor("black")
 
 # user turtle
 u = turtle.Turtle() 
 u.ht() 
 u.left(90)
 u.penup() 
-u.goto(-100, -80)
+u.goto(USER_X, USER_Y)
 u.color("white")
 
 # bot turtle 
@@ -19,7 +30,7 @@ b = turtle.Turtle()
 b.left(90)
 b.ht() 
 b.penup() 
-b.goto(100, 80)
+b.goto(BOT_X, BOT_Y)
 b.color("white")
 
 def show_turtle(t, image): 
@@ -35,23 +46,27 @@ def show_bot(bot):
   
 def update_bot(bot): 
     b.clear()
-    b.goto(-30, 80)
+    b.goto(BOT_X - SLIDE - LUNGE, BOT_Y)
     b.write(bot.health, font=("Arial", 30, "bold"))
-    b.goto(100, 80)
+    b.goto(BOT_X, BOT_Y)
   
 def move_user(): 
-    u.goto(b.pos())
-    u.goto(-100, -80)
+    u.goto(USER_X + LUNGE, USER_Y + LUNGE)
+    u.right(TURN)
+    u.left(TURN)
+    u.goto(USER_X, USER_Y)
   
 def move_bot(): 
-    b.goto(u.pos())
-    b.goto(100, 80)
+    b.goto(BOT_X - LUNGE, BOT_Y - LUNGE)
+    b.left(TURN)
+    b.right(TURN)
+    b.goto(BOT_X, BOT_Y)
   
 def update_user(user): 
     u.clear() 
-    u.goto(0, -80)
+    u.goto(USER_X + SLIDE, USER_Y)
     u.write(user.health, font=("Arial", 30, "bold"))
-    u.goto(-100, -80)
+    u.goto(USER_X, USER_Y)
   
 def draw_update(user, bot): 
     update_user(user)
